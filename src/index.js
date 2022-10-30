c70c7a935e5b92df4afe420b27136c1fcaa0a133
Sántha Kálmán Szakkollégium <santhaszakkoli@Santha-MacBook-Pro.local>
2022-10-30 22:49:39 +0100

    Board setup v2

--- src/index.js
+++ src/index.js
@@ -11,65 +11,73 @@ import {
   RecoilState,
 } from 'recoil';
 
-const startBoard = [
-  'l', 'n', 's', 'g', 'k', 'g', 's', 'n', 'l',
-  '', 'r', '', '', '', '', '', 'b', '',
-  'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 
-  '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '', '', 
-  '', '', '', '', '', '', '', '', '',
-  'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
-  '', 'B', '', '', '', '', '', 'R', '',
-  'L', 'N', 'S', 'G', 'K', 'G', 'S', 'N', 'L',
-]
-
-const squareState = Array(81).fill(null).map((val, ind, arr) => arr[ind] = atom({
-  key: ind.toString(),
-  default: startBoard[ind],
-}))
-
 class Shogi extends React.Component {
   constructor(props) {
     super(props);
 
-  }
-    render() {
-      return (
-          <Board />
-      );
+    this.state = {
+      squares: setUpBoard(),
+      selectedPiece: null,
+      isPlayer1: true
     }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(i) {
+    this.setState({ selectedPiece: i }, () => {
+      console.log(this.state.selectedPiece);
+    });
+  }
+
+  render() {
+    return (
+        <Board 
+          onClick={(i) => this.handleClick(i)} 
+          squares={this.state.squares}
+        />
+    );
+  }
 }
 
 function Square(props) {
-
-  const state = useRecoilValue(squareState[props.i]);
   return (
-      <button className='item'>
+      <button 
+        className='item'
+        onClick={props.onClick}
+      >
         {
-          state !== '' && 
-          <img
-            src={require('../images/' + state.toLowerCase() + '.png')}
-            alt={state}
-            style={{transform: `rotate(${state === state.toLowerCase() ? 180 : 0}deg)`}}
-            >
-          </img>
+          !props.square.isEmpty && setImage(props.square.type, props.square.isPlayer1)
         }
         
-        {/*{state}*/}
       </button>
   );
 }
 
+function setImage(type, isPlayer1) {
+  return (
+    <img
+      src={require('../images/' + type + '.png')}
+      alt={type}
+      style={{transform: `rotate(${!isPlayer1 ? 180 : 0}deg)`}}
+      >
+    </img>
+  )
+}
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
     
   renderSquare(i) {
     return (
       <Square
           i={i}
-          key={i} 
-          //value={this.props.squares[i][j]}
-          //onClick={() => this.props.onClick(i)}  
+          key={i}
+          onClick={() => this.props.onClick(i)}
+          squares={this.props.squares}
+          square={this.props.squares[i]} 
           />
     );
   }
@@ -78,10 +86,61 @@ class Board extends React.Component {
     return (
       <div className="grid-container">
         {
-          squareState.map((v, i) => this.renderSquare(i))
+          this.props.squares.map((v, i) => this.renderSquare(i))
         }
       </div>
     );
+  }
+}
+
+function setUpBoard() {
+  const board = Array(81).fill({
+    isEmpty: true,
+    isSelected: false
+  });
+
+  for (let i = 18; i < 27; i++) {
+    board[i] = createPiece("p", 2);
+  }
+
+  for (let i = 54; i < 63; i++) {
+    board[i] = createPiece("p", 1);
+  }
+
+  board[0] = createPiece("l", 2);
+  board[8] = createPiece("l", 2);
+  board[1] = createPiece("n", 2);
+  board[7] = createPiece("n", 2);
+  board[2] = createPiece("s", 2);
+  board[6] = createPiece("s", 2);
+  board[3] = createPiece("g", 2);
+  board[5] = createPiece("g", 2);
+  board[4] = createPiece("k", 2);
+  board[10] = createPiece("r", 2);
+  board[16] = createPiece("b", 2);
+
+  board[72] = createPiece("l", 1);
+  board[80] = createPiece("l", 1);
+  board[73] = createPiece("n", 1);
+  board[79] = createPiece("n", 1);
+  board[74] = createPiece("s", 1);
+  board[78] = createPiece("s", 1);
+  board[75] = createPiece("g", 1);
+  board[77] = createPiece("g", 1);
+  board[76] = createPiece("k", 1);
+  board[70] = createPiece("r", 1);
+  board[64] = createPiece("b", 1);
+
+  return board;
+}
+
+function createPiece(type, player) {
+  return {
+    isEmpty: false,
+    type: type,
+    isPromoted: false,
+    isPlayer1: player === 1 ? true : false,
+    isSelected: false
   }
 }
 
